5d15bd81eb2b971de91c5d0360bb9c4e28a46c67
Herman Contreras <60008328+hcontr4@users.noreply.github.com>
2024-02-23 03:05:20 +0000

    Players alternate and can dd their pieces to the board

--- board.rb
+++ board.rb
@@ -1,5 +1,6 @@
 # board.rb
 
+# Maintain state of the game
 class Board
   def initialize
     @board = [
@@ -15,5 +16,24 @@ class Board
       puts "| #{@board[row][0]} | #{@board[row][1]} | #{@board[row][2]} |"
     end
       puts '|---|---|---|'
+  end
+
+  def add_x(location)
+    add_piece(location, 'x')
+  end
+
+  def add_o(location)
+    add_piece(location, 'o')
+  end
+
+  def add_piece(location, piece)
+    case location
+    when 7..9
+      @board[0][location - 7] = piece
+    when 4..6
+      @board[1][location - 4] = piece
+    when 1..3
+      @board[2][location - 1] = piece
+    end
   end
 end
--- game.rb
+++ game.rb
@@ -14,9 +14,13 @@ class Game
     loop do 
       do_board
       do_move
+
+      @x_turn = !@x_turn
     end
   end
 
+  private
+  
   def do_board
     puts
     @board.print_board
@@ -24,7 +28,22 @@ class Game
   end
 
   def do_move
-    print 'Move?'
-    move = gets.chomp.to_i
+    move = 0
+    loop do
+      print 'Move for '
+      print @x_turn ? 'x: ' : 'o: '
+      move = gets.chomp.to_i
+
+      break if valid_move?(move)
+
+      puts "\nINVALID MOVE!\n\n"
+    end
+
+    @x_turn ? @board.add_x(move) : @board.add_o(move)
+    @played_locations.append(move)
+  end
+
+  def valid_move?(move)
+    move.between?(1, 9) && !@played_locations.include?(move) ? true : false
   end
 end
